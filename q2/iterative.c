311e82ca267bf30d0d32e5087fd8ca42bd1f7c51
Jungle <wwjd228@gmail.com>
2015-10-15 20:54:56 +0800

    implement of q2 and q4
    Write the q2 with binary search. If find the character in the array,
    return the first character larger than the input character. If not
    found, return the smallest character that is strictly larger than the
    search character. If no such character exists, return the smallest
    character in the array.
    
    Write the q4 with dynamic programming. Find the contiguous subarray
    within an array which has the largest sum.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,45 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
 
+char smallest_character(char str[], char c)
+{
+    unsigned size = 5;
+    unsigned max = size;
+    unsigned min = 0;
+    int smallest = -1;
+    unsigned mid;
+    while ( min < max ) {
+        mid = (min + max) / 2;
+	if (str[mid] == c) {
+	    while (mid < size) {
+	        if (str[mid] > c)
+		    return str[mid];
+
+		mid++;
+	    }
+	}
+
+        else if ( str[mid] < c ) 
+	    min = mid + 1;
+
+	else {
+	    max = mid;
+	    smallest = mid;
+	}
+    }
+    
+    if (smallest == -1)
+        return str[0];
+
+    else
+        return str[smallest];
+}
 int main()
 {
+    char str[] = {'c', 'f', 'j', 'p', 'v'};
+    char smallest_char = smallest_character(str, 'a');
+    printf("The smallest character : %c\n", smallest_char);
+
     return 0;
 }
--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,27 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX( i, j ) ( j > i )? j : i
+
+int maxSubArray(int A[], int n)
+{
+    int max = A[0];
+    int current = A[0];
+    for ( int i = 1 ; i < n ; i++ ) {
+        current = MAX(A[i], A[i] + current);
+	max = MAX(max, current);
+    }
+
+    return max;
+}
 
 int main()
 {
+    int A[] = {-2, -3, 4, -1, -2, 5, 1};
+    int size = 7;
+    int sum = maxSubArray(A, size);
+    printf("max SubArray sum : %d\n", sum);
+
     return 0;
 }
